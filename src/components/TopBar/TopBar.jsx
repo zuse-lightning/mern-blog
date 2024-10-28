1a2889adafd85701500c735f5bd5ac059b6635a8
James Geib <james@zuse.com>
2024-10-28 10:13:07 -0400

    successfully implemented semantic

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,11 +1,11 @@
-
+import { Icon } from "semantic-ui-react";
 import "./style.css";
 
 const TopBar = () => {
     return (
         <div className="top">
             <div className="topLeft">
-                l
+                <Icon name="facebook" size="big" />
             </div>
             <div className="topCenter">c</div>
             <div className="topRight">r</div>
