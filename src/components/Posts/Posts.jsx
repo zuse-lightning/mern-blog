c3cf3dd9b87a3b6e6c8c385b61ef0f49d87ec74e
James Geib <james@zuse.com>
2024-10-28 15:04:33 -0400

    created Posts and SideBar components

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Posts = () => {
+    return (
+        <div className="posts">
+        posts
+        </div>
+    );
+};
+
+export default Posts;
---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const SideBar = () => {
+    return (
+        <div className="sidebar">
+            sidebar
+        </div>
+    );
+};
+
+export default SideBar;
