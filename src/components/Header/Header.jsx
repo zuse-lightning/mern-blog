a5407333ff5b9c4d528251acc09ea171c2887c0c
James Geib <james@zuse.com>
2024-10-28 13:51:51 -0400

    added image to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,13 @@ import "./style.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleSm">React & Node</span>
+            <span className="headerTitleLg">Blog</span>
+        </div>
+        <img className="headerImg" src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?cs=srgb&dl=pexels-artur-roman-158558-1167355.jpg&fm=jpg&_gl=1*19l9s9m*_ga*MjEyNDc5Mjk0OS4xNzMwMTI4NDE4*_ga_8JE65Q40S6*MTczMDEzNjc3Mi4yLjEuMTczMDEzNzc3OS4wLjAuMA.." alt="" />
+    </div>
   );
 };
 
