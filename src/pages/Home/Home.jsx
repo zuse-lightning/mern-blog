c93ce9e09591f44eaf802c59b519034e5676ca11
James Geib <james@zuse.com>
2024-11-12 15:13:15 -0500

    multiple changes to css files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,19 @@
 import Header from "../../components/Header/Header";
+import Posts from "../../components/Posts/Posts";
+import SideBar from "../../components/SideBar/SideBar";
 
 import "./style.css";
 
 const Home = () => {
-  return (
-    <div className="home">
-        <Header />
-        homepage
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <div className="home">
+                <Posts />
+                <SideBar />
+            </div>
+        </>
+    );
 };
 
 export default Home;
